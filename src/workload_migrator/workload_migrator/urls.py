b445fe9f27e63cae347aa7d5e788f6f81aa7d7b6
Mir-Yuchi <mirazimov.kr@gmail.com>
2025-06-15 17:27:44 +0800

    feat(api): register API routes for core viewsets

--- src/workload_migrator/workload_migrator/urls.py
+++ src/workload_migrator/workload_migrator/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from core.views import MigrationTargetViewSet, MigrationViewSet, WorkloadViewSet
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r"workloads", WorkloadViewSet)
+router.register(r"targets", MigrationTargetViewSet)
+router.register(r"migrations", MigrationViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
 ]
